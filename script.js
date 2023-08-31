fc685fdafd40a8fc7414d1824960f11c10698ab6
Maiara Santos <maiarasantos34@yahoo.com>
2023-08-31 13:32:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let display = document.getElementById('display');
 let historyList = document.getElementById('history-list');
+let clearHistoryButton = document.getElementById('clear-history-button');
+let saveHistoryButton = document.getElementById('save-history-button');
+
 let history = [];
 
 function appendToDisplay(value) {
@@ -27,13 +30,27 @@ function calculate() {
       history.pop();
     }
 
+
     updateHistory();
     display.value = result;
+
+    if (history.length >0) {
+      clearHistoryButton.style.display = 'block';
+    } else {
+      clearHistoryButton.style.display = 'button';
+    }
+
   } catch (error) {
     display.value = 'Error';
   }
 }
 
+
+
+function clearHistory() {
+  history = [];
+  updateHistory();
+}
 
 function updateHistory() {
   historyList.innerHTML = '';
@@ -61,4 +78,38 @@ function updateHistory() {
     };
     historyList.appendChild(row);
   }
+  
 }
+
+
+function saveHistoryToFile() {
+  let historyText = '';
+
+  for (let entry of history) {
+    let timestamp = new Date(entry.timestamp);
+    let dayOfWeek = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'][timestamp.getDay()];
+    let month = ('0' + (timestamp.getMonth() + 1)).slice(-2); 
+
+    historyText += `Operação: ${entry.expression}\n`;
+    historyText += `Resultado: ${entry.result}\n`;
+    historyText += `Data: ${dayOfWeek}, ${timestamp.getDate()}/${month}/${timestamp.getFullYear()}, ${timestamp.toLocaleTimeString()}\n\n`;
+  }
+
+  let blob = new Blob([historyText], { type: 'text/plain' });
+  let url = URL.createObjectURL(blob);
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.download = 'history.txt';
+  a.click();
+
+  // Libera a URL do objeto após o download
+  URL.revokeObjectURL(url);
+}
+
+function backspace() {
+  display.value = display.value.slice(0, -1);
+}
+
+
+
